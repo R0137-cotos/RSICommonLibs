96c200ac13aef7e202353c71104d49823f547a6d
Koiwa Yoshiki <z00s600120@rfg.rfgad.ricoh.com>
2019-02-18 16:16:45 +0900

    ライブラリ化

---
+++ src/main/java/jp/co/ricoh/cotos/rsicommonlib/entity/AccountingCreateRsiRunnging/TaxRateInfo.java
@@ -0,0 +1,42 @@
+package jp.co.ricoh.cotos.rsicommonlib.entity.AccountingCreateRsiRunnging;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+/**
+ * 消費税率情報を表すEntity
+ */
+@Entity
+@Data
+public class TaxRateInfo {
+
+	/**
+	 * 行番号
+	 */
+	@Id
+	@JsonProperty(value = "行番号")
+	private long rownum;
+
+	/**
+	 * マスタ名称
+	 */
+	@JsonProperty(value = "マスタ名称")
+	private String articleName;
+
+	/**
+	 * コードの名称
+	 */
+	@JsonProperty(value = "コードの名称")
+	private String displayValue;
+
+	/**
+	 * コード値（消費税率）
+	 */
+	@JsonProperty(value = "コード値（消費税率）")
+	private String codeValue;
+
+}
