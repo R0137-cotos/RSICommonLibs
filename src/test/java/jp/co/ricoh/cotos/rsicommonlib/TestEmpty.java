1d2007f9a12821ac8a751da4c3a2c695729f409d
Asakawa Tomoya <z00s600108@rfg.rfgad.ricoh.com>
2019-02-20 10:43:33 +0900

    空テスト追加

---
+++ src/test/java/jp/co/ricoh/cotos/rsicommonlib/TestEmpty.java
@@ -0,0 +1,18 @@
+package jp.co.ricoh.cotos.rsicommonlib;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class TestEmpty {
+
+	@Test
+	public void test() {
+
+	}
+
+}
